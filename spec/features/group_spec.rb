f68c88b3bd610dbe4028656cbe9b09848f8754df
Temmarie <temmar_whyte16@yahoo.com>
2020-09-04 10:22:09 +0100

    Add more feature test

---
+++ spec/features/group_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe 'Signing In a new user', type: :feature do
+  before(:each) do
+    @user = User.new(
+      email: 'Trish@email.com',
+      password: 'qwerty',
+      password_confirmation: 'qwerty'
+    )
+    @user.save
+    visit new_user_session_path
+
+    fill_in 'user[email]', with: 'Trish@email.com'
+    fill_in 'user[password]', with: 'qwerty'
+
+    click_button 'Log in'
+    expect(page).to have_content('GeeksRead')
+    visit books_path
+    expect(page).to have_content('Add new book')
+  end
+
+  scenario 'add book' do
+    click_on 'Add new book'
+    expect(page).to have_content('New Book')
+  end
+
+  scenario 'add book group' do
+    visit root_path
+    click_on 'Groups'
+    expect(page).to have_content('Groups')
+  end
+
+  scenario 'cannot add group with invalid params' do
+    visit groups_path
+    click_on 'New Group'
+    expect(page).to have_content('Name')
+  end
+
+  scenario 'can add group with valid params' do
+    visit groups_path
+    click_on 'New Group'
+    expect(page).to have_content('Add a new group')
+  end
+end
